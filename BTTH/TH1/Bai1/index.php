d822d98154422f2f74d2b665866851c0f8401b59
TranNgocxX <152256291+TranNgocxX@users.noreply.github.com>
2024-11-30 03:22:45 +0700

    Update index.php

--- BTTH/TH1/Bai1/index.php
+++ BTTH/TH1/Bai1/index.php
@@ -5,13 +5,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title> Chọn Chế Độ hiển Thị </title>
+    <title> Chọn Chế độ hiển thị </title>
 </head>
 <body>
     <h1> Chào mừng đến với Trang Quản Lý Hoa </h1>
     <form method="POST" action="home.php">
         <input type="radio" id="guest" name="mode" value="user" checked>
-        <label for="guest">Người dùng khách</label><br>
+        <label for="user">Người dùng khách</label><br>
         <input type="radio" id="admin" name="mode" value="admin">
         <label for="admin">Quản trị viên</label><br><br>
         <input type="submit" value="Chọn chế độ">
