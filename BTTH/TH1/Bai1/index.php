a4c25502d1415753c4fe66e3efbb4f14e931b3c4
TranNgocxX <152256291+TranNgocxX@users.noreply.github.com>
2024-11-30 03:20:54 +0700

    Create index.php

---
+++ BTTH/TH1/Bai1/index.php
@@ -0,0 +1,20 @@
+<?php
+    include "db_conn.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Chọn Chế Độ hiển Thị </title>
+</head>
+<body>
+    <h1> Chào mừng đến với Trang Quản Lý Hoa </h1>
+    <form method="POST" action="home.php">
+        <input type="radio" id="guest" name="mode" value="user" checked>
+        <label for="guest">Người dùng khách</label><br>
+        <input type="radio" id="admin" name="mode" value="admin">
+        <label for="admin">Quản trị viên</label><br><br>
+        <input type="submit" value="Chọn chế độ">
+    </form>
+</body>
+</html>
