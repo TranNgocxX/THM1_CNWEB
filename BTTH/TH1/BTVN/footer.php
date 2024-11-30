b678a786897d2df836740d0d2d4d3e179e4d6178
TranNgocxX <152256291+TranNgocxX@users.noreply.github.com>
2024-11-30 11:26:44 +0700

    Create footer.php

---
+++ BTTH/TH1/BTVN/footer.php
@@ -0,0 +1,12 @@
+<footer class="bg-white text-dark text-center py-3 mt-4">
+        <hr>
+
+    <div class="container">
+        <p class="mb-0">&copy; 2023 TLU'S MUSIC GARDEN</p> 
+    </div>
+</footer>
+<!-- boostrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
